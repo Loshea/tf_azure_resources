7474c16a32262a03b013b2a13741079ad926b23d
Liam O'Shea <liam.oshea.02@gmail.com>
2023-07-10 20:44:49 +0000

    updating resource group to use budget resource module

--- modules/rg/main.tf
+++ modules/rg/main.tf
@@ -4,6 +4,7 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
+  tags     = var.tags
 }
 
 ######################
@@ -35,62 +36,17 @@ resource "azurerm_resource_group_cost_management_view" "rg_cost_view" {
 # Budget Items
 ######################
 
-#TODO: Variableize the notification block
-#TODO: Define an object for notifications to include contact groups, emails, roles, as well as threshold and operator
-resource "azurerm_monitor_action_group" "rg_monitor" {
-  count               = var.create_budget ? 1 : 0
-  name                = "${var.rg_name}-monitor-group"
-  resource_group_name = azurerm_resource_group.rg.name
-  short_name          = substr("mg-${var.rg_name}", 1, 12)
-}
-
-resource "azurerm_consumption_budget_resource_group" "rg_consumption_budget" {
+module "rg_budget" {
   count             = var.create_budget ? 1 : 0
-  name              = "${var.rg_name}_budget"
-  resource_group_id = azurerm_resource_group.rg.id
+  source            = "git::https://github.com/Loshea/tf_azure_resources.git//modules/budget?ref=v0.11.0"
+  rg_id             = azurerm_resource_group.rg.id
+  budget_name       = "${azurerm_resource_group.rg.name}_${azurerm_resource_group.rg.location}_budget"
+  budget_amount     = var.budget_amount
+  budget_start_date = var.budget_start_date
+  budget_end_date   = var.budget_end_date
+  #TODO Make this part a default and concat with supplied filters
+  filter_dimensions = setunion(toset(var.filter_dimensions), local.rg_filter)
 
-  amount     = var.budget_amount
-  time_grain = "Monthly"
-
-  time_period {
-    start_date = var.budget_start_date
-    end_date   = var.budget_end_date
-  }
-  filter {
-    dimension {
-      name = "ResourceId"
-      values = [
-        azurerm_monitor_action_group.rg_monitor[0].id,
-      ]
-    }
-
-    tag {
-      name = "Purpose"
-      values = [
-        "TFSpendControl",
-      ]
-    }
-  }
-
-  #temporary default notification
-  notification {
-    enabled   = true
-    threshold = 75.0
-    operator  = "EqualTo"
-
-    contact_emails = var.notification_contact_emails
-  }
-
-  dynamic "notification" {
-    for_each = var.notifications
-    content {
-      enabled   = notification.value["enabled"]
-      threshold = notification.value["threshold"]
-      operator  = notification.value["operator"]
-
-      contact_emails = notification.value["contact_emails"]
-      contact_groups = notification.value["contact_groups"]
-      contact_roles  = notification.value["contact_roles"]
-    }
-  }
+  filter_tags   = var.filter_tags
+  notifications = var.notifications
 }
--- modules/rg/outputs.tf
+++ modules/rg/outputs.tf
@@ -12,3 +12,8 @@ output "location" {
   value       = azurerm_resource_group.rg.location
   description = "Region the resource group resides in"
 }
+
+output "budget_name" {
+  value       = module.rg_budget[*].budget_name
+  description = "Name of the associated resource group budget"
+}
