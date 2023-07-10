e22bd3afb6ff5bd15e15907e536cfbf5c3220f60
Liam O'Shea <liam.oshea.02@gmail.com>
2023-07-10 16:53:16 +0000

    Adding budget resource and example

---
+++ modules/budget/examples/main/modules.tf
@@ -0,0 +1,20 @@
+module "example_budget" {
+  source            = "../../"
+  rg_id             = azurerm_resource_group.rg_test.id
+  budget_name       = "tf_rg1_budget"
+  budget_amount     = 100
+  budget_start_date = "2023-07-01T00:00:00Z"
+  filter_dimensions = [{
+    name   = "ResourceGroupName"
+    values = [azurerm_resource_group.rg_test.name]
+  }]
+  notifications = [{
+    enabled        = true
+    threshold      = 85.0
+    operator       = "GreaterThan"
+    contact_emails = ["liam.oshea.02@gmail.com"]
+    contact_emails = ["liam.oshea.02@gmail.com"]
+    contact_roles  = ["Owner"]
+    contact_groups = []
+  }]
+}
---
+++ modules/budget/examples/main/provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  #These should be provided as environment variables
+  # subscription_id   = "<azure_subscription_id>"
+  # tenant_id         = "<azure_subscription_tenant_id>"
+  # client_id         = "<service_principal_appid>"
+  # client_secret     = "<service_principal_password>"
+
+}
