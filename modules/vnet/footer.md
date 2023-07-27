abe77ad39c647d77788bfe3676e78350065fb4e3
Liam O'Shea <liam.oshea.02@gmail.com>
2023-07-27 13:29:37 +0000

    Testing footer from main.tf

--- modules/vnet/footer.md
+++
@@ -1,4 +0,0 @@
-# Examples
-The [example](examples/main/) demonstrates use of this module to create a Virtual Network containing 3 subnets of different sizes.
-
-The [create bastion](examples/create_bastion/) demonstrates the addition of a bastion host.
--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -81,3 +81,10 @@ resource "azurerm_bastion_host" "vnetbastion" {
 
   tags = var.tags
 }
+
+/**
+* # Examples
+* The [example](examples/main/) demonstrates use of this module to create a Virtual Network containing 3 subnets of different sizes.
+*
+* The [create bastion](examples/create_bastion/) demonstrates the addition of a bastion host.
+*/
