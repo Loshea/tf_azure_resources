9c6048903433b7d6bf72b57edc33d9dcc5603162
Liam O'Shea <liam.oshea.02@gmail.com>
2023-07-27 15:42:10 +0000

    Updating example references

---
+++ modules/rg/examples/rg_with_budget/modules.tf
@@ -0,0 +1,19 @@
+
+module "rg1" {
+  source            = "git::https://github.com/Loshea/tf_azure_resources.git//modules/rg?ref=v0.14.0"
+  rg_name           = "tf_rg1"
+  location          = "East US"
+  create_costview   = true
+  create_budget     = true
+  budget_amount     = 100
+  budget_start_date = "2023-07-01T00:00:00Z"
+  notifications = [{
+    enabled        = true
+    threshold      = 85.0
+    operator       = "GreaterThan"
+    contact_emails = ["liam.oshea.02@gmail.com"]
+    contact_emails = ["liam.oshea.02@gmail.com"]
+    contact_roles  = ["Owner"]
+    contact_groups = []
+  }]
+}
--- modules/rg/examples/rg_with_budget/outputs.tf
+++ modules/rg/examples/rg_with_budget/outputs.tf
@@ -7,3 +7,8 @@ output "location" {
   value       = module.rg1.location
   description = "Location the new resource group was created in"
 }
+
+output "budget_name" {
+  value       = module.rg1.budget_name
+  description = "Name of the budget associated with the resource grouo"
+}
